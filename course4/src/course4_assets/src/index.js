04152ce6f3ddfc18e46d04f37817c333ca40cc25
feilibaby@163.com <feilibaby@163.com>
2022-06-19 20:56:37 +0800

    feat:add course4 homework

---
+++ course4/src/course4_assets/src/index.js
@@ -0,0 +1,19 @@
+import { course4 } from "../../declarations/course4";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await course4.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
