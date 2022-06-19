43949634e375ac942aeea6e76957e7e5d59ab324
feilibaby@163.com <feilibaby@163.com>
2022-06-19 22:16:34 +0800

    feat:add course4 homework

--- course4/src/declarations/course4/course4.did.d.ts
+++ course4/src/declarations/course4/course4.did.d.ts
@@ -1,4 +1,26 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export interface _SERVICE { 'greet' : ActorMethod<[string], string> }
+export type OperationType = { 'addRestricted' : null } |
+  { 'stop' : null } |
+  { 'removeRestricted' : null } |
+  { 'delete' : null } |
+  { 'create' : null } |
+  { 'start' : null } |
+  { 'install' : null };
+export type Wasm_module = Array<number>;
+export interface anon_class_18_1 {
+  'add_restricted' : ActorMethod<[Principal], undefined>,
+  'create_canister' : ActorMethod<[], [] | [Principal]>,
+  'delete_canister' : ActorMethod<[Principal], undefined>,
+  'install_code' : ActorMethod<[Principal, [] | [Wasm_module]], undefined>,
+  'make_proposal' : ActorMethod<
+    [OperationType, Principal, [] | [Wasm_module]],
+    undefined,
+  >,
+  'remove_restricted' : ActorMethod<[Principal], undefined>,
+  'start_canister' : ActorMethod<[Principal], undefined>,
+  'stop_canister' : ActorMethod<[Principal], undefined>,
+  'vote_proposal' : ActorMethod<[bigint, boolean], undefined>,
+}
+export interface _SERVICE extends anon_class_18_1 {}
--- course4/src/declarations/course4/course4.did.js
+++ course4/src/declarations/course4/course4.did.js
@@ -1,4 +1,31 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], []) });
+  const Wasm_module = IDL.Vec(IDL.Nat8);
+  const OperationType = IDL.Variant({
+    'addRestricted' : IDL.Null,
+    'stop' : IDL.Null,
+    'removeRestricted' : IDL.Null,
+    'delete' : IDL.Null,
+    'create' : IDL.Null,
+    'start' : IDL.Null,
+    'install' : IDL.Null,
+  });
+  const anon_class_18_1 = IDL.Service({
+    'add_restricted' : IDL.Func([IDL.Principal], [], ['oneway']),
+    'create_canister' : IDL.Func([], [IDL.Opt(IDL.Principal)], []),
+    'delete_canister' : IDL.Func([IDL.Principal], [], []),
+    'install_code' : IDL.Func([IDL.Principal, IDL.Opt(Wasm_module)], [], []),
+    'make_proposal' : IDL.Func(
+        [OperationType, IDL.Principal, IDL.Opt(Wasm_module)],
+        [],
+        [],
+      ),
+    'remove_restricted' : IDL.Func([IDL.Principal], [], ['oneway']),
+    'start_canister' : IDL.Func([IDL.Principal], [], []),
+    'stop_canister' : IDL.Func([IDL.Principal], [], []),
+    'vote_proposal' : IDL.Func([IDL.Nat, IDL.Bool], [], []),
+  });
+  return anon_class_18_1;
 };
-export const init = ({ IDL }) => { return []; };
+export const init = ({ IDL }) => {
+  return [IDL.Nat, IDL.Nat, IDL.Vec(IDL.Principal)];
+};
